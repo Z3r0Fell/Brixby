383c96a62c0aa952fd1e744d3c84d16b7003f004
ZeroFell <130316675+Z3r0Fell@users.noreply.github.com>
2025-12-02 18:37:02 -0500

    Initial project configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        frost: '#dff3ff',
+        ice: '#e9f7ff',
+        storm: '#2f3b4e'
+      }
+    }
+  },
+  plugins: []
+}
